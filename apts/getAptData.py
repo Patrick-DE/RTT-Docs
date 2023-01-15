c251870d1309be5abc638fb79f702f94fdf250d3
Patrick-DE <patrick.eisenschmidt@gmx.de>
2023-01-15 15:14:13 +0100

    added minified version of apts for performance reasons

--- apts/getAptData.py
+++ apts/getAptData.py
@@ -8,9 +8,15 @@ class APT:
     self.aliases = aliases
     self.techniques = techniques
 
-  def toJSON(self):
-    return json.dumps(self, default=lambda o: o.__dict__, 
-        sort_keys=True, indent=4)
+  # def toJSON(self):
+  #   return json.dumps(self, default=lambda o: o.__dict__, 
+  #       sort_keys=True, indent=4)
+
+class APT_min:
+  def __init__(self, name, techniques):
+    self.name = name
+    self.techniques = techniques
+
 
 # lift = attack_client()
 # groups = lift.get_groups()
@@ -23,12 +29,14 @@ techniques_used = lift.get_techniques_used_by_all_groups()
 groups = lift.get_groups()
 #groups = lift.remove_revoked(groups)
 apt_list = []
+apt_list_min = []
 
 for g in groups:
   if g["revoked"]:
     continue
 
   techniques_list = []
+  techniques_list_min = []
 
   for gut in techniques_used:
     if gut["revoked"]:
@@ -47,13 +55,18 @@ for g in groups:
         if hasattr(refs, "url") or "url" in refs:
           technique_dict['group_ref'].append(refs["url"])
       techniques_list.append(technique_dict)
+      techniques_list_min.append({'techniqueId': gut['technique_id']})
 
   aliases = [gut['name']]
   if hasattr(g, "aliases"):
     aliases = g["aliases"]
 
   apt_list.append(APT(g['name'], g["description"], aliases, techniques_list))
+  apt_list_min.append(APT_min(g['name'], techniques_list_min))
 
 
 with open("apts.json", "w") as f:
-  json_string = json.dump([apt.__dict__ for apt in apt_list], f)
+  json.dump([apt.__dict__ for apt in apt_list], f)
+
+with open("apts_min.json", "w") as f:
+  json.dump([apt.__dict__ for apt in apt_list_min], f)
